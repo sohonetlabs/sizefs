26fad16ac64d58e95a8930671c41bb4a18f2aff7
Joel Wright <joel.wright@sohonet.com>
2013-10-11 13:35:35 +0200

    more tests

--- tests/test_contents.py
+++ tests/test_contents.py
@@ -1,13 +1,14 @@
 __author__ = 'jjw'
 
-import sys
 import os
+import re
+import sys
+import unittest
+
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from sizefs.contents import XegerGen
-import re
-import unittest
 
 
 class XegerGenTestCase(unittest.TestCase):
@@ -16,6 +17,14 @@ class XegerGenTestCase(unittest.TestCase):
         generator = XegerGen(1024, filler="0", max_random=10)
         contents = generator.read(0, 15)
         assert contents == "0000000000000000"
+
+    def test_padding(self):
+        generator = XegerGen(64, filler="55555", max_random=10)
+        contents = generator.read(0, 63)
+        assert contents.endswith("50000")
+        generator = XegerGen(64, filler="55555", padder="longer", max_random=10)
+        contents = generator.read(0, 63)
+        assert contents.endswith("5long")
 
     def test_prefix(self):
         generator = XegerGen(1024, prefix="11", filler="0", max_random=10)
@@ -51,7 +60,7 @@ class XegerGenTestCase(unittest.TestCase):
 
     def test_choice(self):
         generator = XegerGen(1024, filler="a[012345]{14}b", max_random=10)
-        contents = generator.read(0, 256)
+        contents = generator.read(0, 15)
         match = re.match("a[012345]{14}b", contents)
         assert match is not None
 
@@ -60,6 +69,7 @@ class XegerGenTestCase(unittest.TestCase):
         contents = generator.read(0, 256)
         match = re.match("a[0-9,a-z,A-Z]{5}d", contents)
         assert match is not None
+
 
 if __name__ == '__main__':
     unittest.main()
